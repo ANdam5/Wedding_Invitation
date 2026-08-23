b298771dfabe6a6f3e148fe773b54632b1715c4f
ANdam5 <kfcmbrd@naver.com>
2026-08-23 17:18:48 +0900

    Map key + 간단한 내용 변경

--- src/const.ts
+++ src/const.ts
@@ -14,30 +14,30 @@ export { dayjs }
  * 예식 일시 설정
  * Asia/Seoul 타임존 기준으로 설정합니다.
  */
-export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
+export const WEDDING_DATE = dayjs.tz("2027-01-30 15:30", "Asia/Seoul")
 
 /**
  * 예식 일시 포맷
  * 분이 0이면 분을 생략하고, 그 외에는 표시합니다.
  * 예: 2024년 8월 24일 토요일 오후 1시
  */
-export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
+export const WEDDING_DATE_FORMAT = `2027년 1월 30일 토요일 오후 2시${WEDDING_DATE.minute() === 0 ? "" : " 30분"}`
 
 /**
  * 예식 당월 휴무일 (달력 표시용)
  * 예: 8월 15일 광복절
  */
-export const HOLIDAYS = [15]
+export const HOLIDAYS = [1]
 
 /**
  * 예식 장소 명칭
  */
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
+export const LOCATION = "국방컨벤션"
 
 /**
  * 예식 장소 상세 주소
  */
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION_ADDRESS = "서울특별시 용산구 용산동3가 이태원로 22"
 
 /**
  * 카카오톡 공유 시 사용할 위치 정보 주소
@@ -53,26 +53,26 @@ export const SHARE_ADDRESS_TITLE = LOCATION
 /**
  * 지도 서비스(네이버, 카카오)에 사용할 좌표 [경도, 위도]
  */
-export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
+export const WEDDING_HALL_POSITION = [126.9777, 37.5336]
 
 /**
  * 네이버 지도 장소 ID (NMAP_PLACE_ID)
  * 네이버 지도에서 장소 검색 후 URL의 숫자 부분을 입력합니다.
  */
-export const NMAP_PLACE_ID = 13321741
+export const NMAP_PLACE_ID = 13353325
 
 /**
  * 카카오 지도 장소 ID (KMAP_PLACE_ID)
  * 카카오 지도에서 장소 상세보기 클릭 후 URL의 숫자 부분을 입력합니다.
  */
-export const KMAP_PLACE_ID = 8634826
+export const KMAP_PLACE_ID = 152313844
 
 // 신부 정보 설정
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
-export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_FULLNAME = "ABC"
+export const BRIDE_FIRSTNAME = "BC"
+export const BRIDE_TITLE = "차녀"
+export const BRIDE_FATHER = "아버지"
+export const BRIDE_MOTHER = "어머니"
 
 /**
  * 신부측 연락처 및 계좌 정보
@@ -99,11 +99,11 @@ export const BRIDE_INFO = [
 ]
 
 // 신랑 정보 설정
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
+export const GROOM_FULLNAME = "ABC"
+export const GROOM_FIRSTNAME = "BC"
 export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FATHER = "아버지"
+export const GROOM_MOTHER = "어머니"
 
 /**
  * 신랑측 연락처 및 계좌 정보
